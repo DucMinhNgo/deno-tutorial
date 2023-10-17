9bb4b175553f7d16b4674f3ce8b81267075d0de2
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-10-17 14:55:38 +0700

    docker build mysql

--- read_env.ts
+++ read_env.ts
@@ -3,5 +3,9 @@ import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 const env = await load();
 const password = env["PASSWORD"];
 
+debugger;
+
 console.log(password);
+console.log(env["DENO_AUTH_TOKENS"]);
+
 // "Geheimnis"
